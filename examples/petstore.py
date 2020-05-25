3a77491b92580a1f244a15c13bb14b9a11c18a6c
imthor <48450247+imthor@users.noreply.github.com>
2020-05-25 22:19:51 +0000

    updated the petstore example and ran black

--- examples/petstore.py
+++ examples/petstore.py
@@ -9,14 +9,16 @@ def main():
     """
     Pet Store runner
     """
+
     def path_prehook(path):
-        return path.replace('/pet/', '/')
+        return path.replace("/pet/", "/")
+
     option = {
-        #'exclude_path_regex': ['store'],
+        "exclude_path_regex": ["/user/post"],
         #'include_path_regex': ['get'],
-        'prehooks': {
+        "prehooks": {
             #'path': path_prehook
-        }
+        },
     }
 
     swag = Swagcli("https://petstore.swagger.io/v2/swagger.json", **option)
