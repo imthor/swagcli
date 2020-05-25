cf63b638f48941288d21cf33f4a6205fed522653
Ubuntu <ubuntu@ip-172-31-46-135.ap-south-1.compute.internal>
2020-05-25 09:02:28 +0000

    add bin and setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="swagcli",
+    version="0.1",
+    description="Get easy click cli for your api using swagger config",
+    author="imthor",
+    license="MIT",
+    packages=["swagcli"],
+    scripts=["bin/cli"],
+)
